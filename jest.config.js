305a9069a46a2d3c121df7469487cc166b74b19a
Rai <wrsilva@cleartech.com.br>
2024-09-27 19:28:31 -0300

    ajuste workflow

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
 };
